8295becbb3f5dff3fb4e253b734716e6714675af
+ <+>
2023-08-03 23:19:57 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -14,19 +14,29 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages', methods=["GET", "POST"])
+@app.route('/messages', methods = ["GET", "POST"])
 def messages():
     if request.method == "GET":
-        all = Message.query.order_by('created_at').all()
+        messages_list = []
+        messages = Message.query.order_by(Message.created_at.desc()).all()
+        for message in messages:
+            message_dict = {
+                "id": message.id,
+                "body": message.body,
+                "username": message.username,
+                "created_at": message.created_at,
+                "updated_at": message.updated_at,
+            }
+            messages_list.append(message_dict)
 
         response = make_response(
-            jsonify([message.to_dict() for message in all]),
-            200
+            jsonify(messages_list),
+            200,
+            {"Content_Type": "application/json"}
         )
-
         return response
     
-    if request.method == "POST":
+    elif request.method == "POST":
         new = request.get_json()
         message = Message(
             body=new['body'],
@@ -44,21 +54,20 @@ def messages():
         return response
 
 
-@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
+@app.route('/messages/<int:id>', methods = ["PATCH", "DELETE"])
 def messages_by_id(id):
     message = Message.query.filter_by(id=id).first()
+    if request.method == "PATCH":
+        messages = request.get_json()
+        for attr in messages:
+            setattr(message, attr, messages[attr])
 
-    if request.method == 'PATCH':
-        data = request.get_json()
-        for attr in data:
-            setattr(message, attr, data[attr])
-            
         db.session.add(message)
         db.session.commit()
 
         response = make_response(
             jsonify(message.to_dict()),
-            200,
+            200
         )
 
     elif request.method == 'DELETE':
